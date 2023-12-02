2fe56dfaf8724921eb91c0f268c28307d0013273
Dan Hill <danshill@gmail.com>
2023-12-02 10:16:34 -0700

    adding some of the optional exercizes

--- src/further-study.js
+++ src/further-study.js
@@ -6,14 +6,34 @@
 //   => ['a']
 //   commonStrings(['zoo', 'space', 'zoo'], ['zoo', 'space', 'boat']);
 //   => ['zoo', 'space']
-function commonStrings(array1, array2) {}
+function commonStrings(array1, array2) {
+    const matchedStrings = [];
+    for (let word of array1) {
+        if (array2.includes(word)) {
+            matchedStrings.push(word);
+        }
+    }
+    return matchedStrings;
+}
 
 // Given three numbers, return a list of numbers from 1 to 100 that are
 // divisible by at least one of a, b or c.
 // Ex.:
 //   divisibleByEither(50, 30, 29);
 //   => [29, 30, 50, 58, 60, 87, 90, 100]
-function divisibleByEither(a, b, c) {}
+function divisibleByEither(a, b, c) {
+    const divisible = [];
+    for (let i = 1; i < 101; i++) {
+        if ((i%a) === 0) {
+            divisible.push(i);
+        } else if ((i%b) === 0) {
+            divisible.push(i);
+        } else if ((i%c) === 0) {
+            divisible.push(i);
+        }
+    }
+    return divisible;
+}
 
 // Compress a string using the rules below and return the result. To compress a
 // string, replace consecutive duplicate characters with a number and the
@@ -27,6 +47,28 @@ function divisibleByEither(a, b, c) {}
 //   => '4s2b'
 //   compressString('ssssbbba');
 //   => '4s3ba'
-function compressString(string) {}
+function compressString(string) {
+    let newString = '';
+    let currChar = string[0];
+    let prevChar = '';
+    let currCount = 1;
+  
+    for (let i = 1; i < string.length; i++) {
+      prevChar = currChar;
+      currChar = string[i];
+      if (currChar === prevChar) {
+        currCount++;
+      } else {
+        if (currCount > 1) {
+          newString += currCount + prevChar;
+          currCount = 1;
+        } else {
+          newString += currChar;
+        }
+      }
+    }
+    console.log(newString);
+    return newString;
+  }
 
 export { commonStrings, compressString, divisibleByEither };
